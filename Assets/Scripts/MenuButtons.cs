59f4f09c6ee0278f0ca2119a7a61957f77538e79
j8cov <j8cobcreative@gmail.com>
2023-07-10 16:40:41 -0400

    Menu Updates

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -4,10 +4,48 @@ using UnityEngine;
 
 public class MenuButtons : MonoBehaviour
 {
+    public GameObject controlls;
+    public GameObject settings;
+
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    public void OnClickStartGame()
+    {
+        //load Level One// SceneManager.LoadScene(1);
+
+    }
+    public void OnClickControlls()
+    {
+        if (controlls.activeSelf || settings.activeSelf)
+        {
+            controlls.SetActive(false);
+        }
+        else
+        {
+            controlls.SetActive(true);
+        }
+        
+    }
+    public void OnClickSettings()
+    {
+        if (settings.activeSelf || controlls.activeSelf)
+        {
+            settings.SetActive(false);
+        }
+        else
+        {
+            settings.SetActive(true);
+        }
+
+    }
+
+    public void OnClickQuit()
+    {
+        Application.Quit();
     }
 
     // Update is called once per frame
